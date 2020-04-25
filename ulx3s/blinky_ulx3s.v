ad072cb8e173485b5e5f58e7a81860c36f0d6705
Olof Kindgren <olof.kindgren@gmail.com>
2020-04-25 23:04:22 +0200

    Fix nextpnr options and ulx3s targets

--- ulx3s/blinky_ulx3s.v
+++ ulx3s/blinky_ulx3s.v
@@ -1,10 +1,10 @@
 module blinky_ulx3s
   (input wire 	    clk,
-   input wire [0:0] btn,
+   input wire       btn0,
    output wire 	    wifi_gpio0,
    output wire 	    q);
 
-   assign wifi_gpio0 = btn[0];
+   assign wifi_gpio0 = btn0;
 
    blinky #(.clk_freq_hz (25_000_000)) blinky
      (.clk (clk),
