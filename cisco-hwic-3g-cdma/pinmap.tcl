baeeb76771f01e9e92c305d32f9d61bc7299fd98
Tom Verbeure <hombre+github@gmail.com>
2020-05-11 21:11:12 -0700

    Set unused pins it input.

--- cisco-hwic-3g-cdma/pinmap.tcl
+++ cisco-hwic-3g-cdma/pinmap.tcl
@@ -9,3 +9,7 @@ set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to clk
 #
 set_location_assignment PIN_AA19 -to q
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to q
+
+# Actively driving unassigned IOs make this FPGA board very hot. Don't do it.
+set_global_assignment -name RESERVE_ALL_UNUSED_PINS "AS INPUT TRI-STATED WITH WEAK PULL-UP"
+
