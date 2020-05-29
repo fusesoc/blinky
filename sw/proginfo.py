0a720196c9465e024ca87affd0c4be8c518d6b71
Olof Kindgren <olof.kindgren@gmail.com>
2020-05-29 13:58:18 +0200

    Prepare for release

--- sw/proginfo.py
+++ sw/proginfo.py
@@ -1,14 +1,15 @@
 import os
 import sys
 
+system_name = "fusesoc_utils_blinky_1.0"
 print("")
 print("Build was completed")
 print("")
 print("To program the board run:")
 if sys.argv[1] == 'tinyprog':
-    print("tinyprog --program {}".format(os.path.join(os.getcwd(), "fusesoc_utils_blinky_0.bin")))
+    print("tinyprog --program {}".format(os.path.join(os.getcwd(), system_name+".bin")))
 elif sys.argv[1] == 'iceprog':
-    print("iceprog {}".format(os.path.join(os.getcwd(), "fusesoc_utils_blinky_0.bin")))
+    print("iceprog {}".format(os.path.join(os.getcwd(), system_name+".bin")))
 elif sys.argv[1] == 'ujprog':
-    print("ujprog {}".format(os.path.join(os.getcwd(), "Implementation0", "fusesoc_utils_blinky_0_Implementation0.bit")))
+    print("ujprog {}".format(os.path.join(os.getcwd(), "Implementation0", system_name+"_Implementation0.bit")))
     print("Other programming options are listed here https://github.com/emard/ulx3s/blob/master/doc/MANUAL.md#programming-options")
