dd491fc248a3a513b5f65ee5cbe9357e361ccd54
Dave Dribin <dave-vcs@dribin.org>
2021-07-03 15:36:43 -0500

    Correct Go Board clock frequency to 25MHz
    Make all LED output names consistent in the RTL and  PCF.

--- go_board/blinky_go_board.v
+++ go_board/blinky_go_board.v
@@ -1,18 +1,19 @@
 module blinky_go_board
   #(parameter clk_freq_hz = 0)
-   (input  clk,
-    output reg q = 1'b0,
-    output led2 = 1'b0,
-    output led3 = 1'b0,
-    output led4 = 1'b0);
+   (input      clk,
+    output reg led1 = 1'b0,
+    // Prevent the other three LEDs from floating
+    output     led2 = 1'b0,
+    output     led3 = 1'b0,
+    output     led4 = 1'b0);
 
    reg [$clog2(clk_freq_hz)-1:0] count = 0;
 
    always @(posedge clk) begin
       count <= count + 1;
       if (count == clk_freq_hz-1) begin
-	 q <= !q;
-	 count <= 0;
+         led1 <= !led1;
+         count <= 0;
       end
    end
 
