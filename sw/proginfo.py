34755694b172cf5750e2affa79a043b07889157a
Jansen Arruda <jansen.arruda@gmail.com>
2021-10-22 09:04:42 -0300

    Update to add icesugar
    Add icesugar programmer (icesprog)

--- sw/proginfo.py
+++ sw/proginfo.py
@@ -10,6 +10,8 @@ if sys.argv[1] == 'tinyprog':
     print("tinyprog --program {}".format(os.path.join(os.getcwd(), system_name+".bin")))
 elif sys.argv[1] == 'iceprog':
     print("iceprog {}".format(os.path.join(os.getcwd(), system_name+".bin")))
+elif sys.argv[1] == 'icesprog':
+    print("icesprog {}".format(os.path.join(os.getcwd(), system_name+".bin")))
 elif sys.argv[1] == 'ujprog':
     print("ujprog {}".format(os.path.join(os.getcwd(), "Implementation0", system_name+"_Implementation0.bit")))
     print("Other programming options are listed here https://github.com/emard/ulx3s/blob/master/doc/MANUAL.md#programming-options")
