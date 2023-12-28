d4ebce0ec082aa956eb1c7456fd7f8e2364b68a0
Liam Beguin <liambeguin@gmail.com>
2023-12-27 20:11:11 -0500

    polarfire_splashkit: add all LED lines
    Add all LED lines and pull them to GND so they are truly off.
    Also fix formatting, and align statements.
    
    Signed-off-by: Liam Beguin <liambeguin@gmail.com>

--- polarfire_splashkit/blinky_polarfire_splashkit.v
+++ polarfire_splashkit/blinky_polarfire_splashkit.v
@@ -1,9 +1,23 @@
-module blinky_polarfire_splashkit
-  (input wire 	    clk,
-   output wire 	    q);
+module blinky_polarfire_splashkit (
+	input	wire	i_clk,
+	output	wire	o_led1 = 1'b0,
+	output	wire	o_led2 = 1'b0,
+	output	wire	o_led3 = 1'b0,
+	output	wire	o_led4,
+	output	wire	o_led5 = 1'b0,
+	output	wire	o_led6 = 1'b0,
+	output	wire	o_led7 = 1'b0,
+	output	wire	o_led8 = 1'b0);
 
-   blinky #(.clk_freq_hz (50_000_000)) blinky
-     (.clk (clk),
-      .q   (q));
+	wire q;
+
+	blinky #(
+		.clk_freq_hz (50_000_000)
+	) blinky (
+		.clk (i_clk),
+		.q   (q)
+	);
+
+	assign o_led4 = q;
 
 endmodule
