88a9f27a0da6ec9a86a6bf44428da2e59c90e1a0
Greg Steiert <greg@steiert.net>
2024-06-15 17:56:31 -0700

    adding JBC generation to MAX10 10M08 Evaluation Kit

--- max10_10m08evk/pinmap.tcl
+++ max10_10m08evk/pinmap.tcl
@@ -12,3 +12,6 @@ set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to q
 
 # Generate SVF File for openFPGALoader
 set_global_assignment -name GENERATE_SVF_FILE ON
+# Generate JAM file to be converted to JBC with quartus_jbcc
+set_global_assignment -name GENERATE_JAM_FILE ON
+
